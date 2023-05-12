c958307f94b20ee2c1b605c505070d9b3b37ae3d
Alejandro H Tadino M <tadinomalejandroh@gmail.com>
2023-05-12 15:21:39 -0400

    [ADD] Surround and other plugins

--- mappings.lua
+++ mappings.lua
@@ -8,10 +8,14 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+    ["<Tab>"] = { "<cmd>bnext<CR>", desc = "Next Buffer" },
+    ["<S-Tab>"] = { "<cmd>bprev<CR>", desc = "Previous Buffer" },
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,5 @@
+-- TODO: Test
+local prefix = "<leader>x"
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -9,4 +11,56 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {},
+    event = "User AstroFile",
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "Wansmer/treesj",
+    keys = {
+      {
+        "<leader>m",
+        "<CMD>TSJToggle<CR>",
+        desc = "Toggle Treesitter Join",
+      },
+    },
+    cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
+    opts = { use_default_keymas = false },
+  },
+  {
+    "kevinhwang91/nvim-bqf",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble" },
+    keys = {
+      { prefix, desc = "Trouble" },
+      { prefix .. "X", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics" },
+      { prefix .. "x", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics" },
+      { prefix .. "q", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List" },
+    },
+    opts = {
+      use_diagnostic_signs = true,
+      actions_keys = {
+        close = { "q", "<esc>" },
+        cancel = "<c-e>",
+      },
+    },
+  },
 }
